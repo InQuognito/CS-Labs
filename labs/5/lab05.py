3ec9fa5e1fa44b853fd93972c5abd9a44ff39c3f
Patrick Cotter <44120299+InQuognito@users.noreply.github.com>
2022-09-30 10:53:49 -0500

    Add files via upload

--- labs/5/lab05.py
+++ labs/5/lab05.py
@@ -1,32 +1,57 @@
+def get_school(card):
+	if int(card[5]) == 1:
+		return 'School of Computing and Engineering SCE'
+	elif int(card[5]) == 2:
+		return 'School of Law'
+	elif int(card[5]) == 3:
+		return 'College of Arts and Sciences'
+	else:
+		return 'Invalid School'
+
+def get_grade(card):
+	if int(card[6]) == 1:
+		return 'Freshman'
+	elif int(card[6]) == 2:
+		return 'Sophomore'
+	elif int(card[6]) == 3:
+		return 'Junior'
+	elif int(card[6]) == 4:
+		return 'Senior'
+	else:
+		return 'Invalid Grade'
+
 def character_value(checkChar):
 	convertedChar = ord(checkChar) - 65
 	return convertedChar
 
 def get_check_digit(card):
-    checkDigit = 0
-    for inx, digit in enumerate(card[:5], 1):
-        print(ord(digit) - 65)
-        checkDigit += (inx + 1) * (ord(digit) - 65)
-    print(checkDigit % 10)
+	checkDigit = 0
+	for index, value in enumerate(card[:5], 0):
+		checkDigit += ((index + 1) * (ord(value) - 65))
 
-    for inx, digit in enumerate(card[:5], 1):
-        print(ord(digit) - 48)
-        checkDigit += (inx + 1) * digit
-    
-    return checkDigit % 10
+	for index, value in enumerate(card[5:9], 5):
+		checkDigit += ((index + 1) * int(value))
 
-def check_name(id):
-	for char in id[0:5]:
-		if ord(id[char]) not in range(0,26):
-			return False, char, chr(id[char])
+	checkDigit %= 10
+	return checkDigit
 
-def check_index_5(id):
-	if ord(id[5]) not in range(1,4):
-		return False
-
-def check_index_6(id):
-	if ord(id[6]) not in range(1,5):
-		return False
+def verify_check_digit(card):
+	if len(card) != 10:
+		return False, 'The length of the number given must be 10'
+	for index, value in enumerate(card[0:5]):
+		if (ord(value) - 65) not in range(0,26):
+			return False, f'The first 5 characters must be A-Z, the invalid character is at {index} is {value}'
+	if int(card[5]) not in range(1,4):
+		return False, 'The sixth character must be 1 2 or 3'
+	if int(card[6]) not in range(1,5):
+		return False, 'The seventh character must be 1 2 3 or 4'
+	for index, value in enumerate(card[7:10]):
+		if int(value) not in range(10):
+			return False, f'The last 3 characters must be 0-9, the invalid character is at {index} is {value}'
+	checkDigit = get_check_digit(card)
+	if checkDigit != int(card[9]):
+		return False, f'Check Digit {card[9]} does not match calculated value {checkDigit}.'
+	return True, ''
 
 def check_card():
 	print('Linda Hall')
@@ -35,15 +60,19 @@ def check_card():
 
 	card = input('Enter Library Card ID. Hit Enter to Exit ==> ')
 
-	if len(card) != 10:
-		print('ERROR: The length of the number must be 10.')
-	condition, errorIndex, error = check_name(card)
-	if condition == False:
-		print(f'ERROR: The first 5 characters must be A-Z. The invalid character at {errorIndex} is {error}') # TODO: Return specific information about the invalid character.
-	if check_index_5(card) == False:
-		print('ERROR: The sixth character must be 1-3.') # TODO: Return specific information about the invalid character.
-	if check_index_6(card) == False:
-		print('ERROR: The seventh character must be 1-4.') # TODO: Return specific information about the invalid character.
+	if card == '':
+		pass
+	else:
+		verified, error = verify_check_digit(card)
+
+		if verified == False:
+			print(error)
+		else:
+			print(f'The card belongs to a student in {get_school(card)}')
+			print(f'The card belongs to a {get_grade(card)}')
+
+			print()
+			check_card()
 
 if __name__ == '__main__':
 	check_card()
